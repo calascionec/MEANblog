bbc9ea5ca24c7a1a2a023c931b6ee2ab2c1d8bfb
calascionec <chris.calascione@gmail.com>
2016-01-16 13:45:51 -0500

    add routes for GETing a single post

--- routes/posts.js
+++ routes/posts.js
@@ -41,5 +41,48 @@ router.route('/')
       }
     })
   })
+  // route middleware to validate :id
+router.param('id', function(req, res, next, id) {
+    //console.log('validating ' + id + ' exists');
+    //find the ID in the Database
+    mongoose.model('post').findById(id, function (err, post) {
+        //if it isn't found, we are going to repond with 404
+        if (err) {
+            console.log(id + ' was not found');
+            res.status(404)
+            var err = new Error('Not Found');
+            err.status = 404;
+            res.format({
+                html: function(){
+                    next(err);
+                 },
+                json: function(){
+                       res.json({message : err.status  + ' ' + err});
+                 }
+            });
+        //if it is found we continue on
+        } else {
+            //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
+            //console.log(blob);
+            // once validation is done save the new item in the req
+            req.id = id;
+            // go to the next thing
+            next();
+        }
+    });
+});
+//return a single post
 
-  module.exports = router;
+router.route('/:id')
+  .get(function(req, res) {
+    mongoose.model('post').findById(req.id, function (err, post) {
+      if (err) {
+        console.log('GET Error: There was a problem retrieving: ' + err);
+      } else {
+        console.log('GET Retrieving ID: ' + post._id);
+        res.json(post);
+        }
+      })
+    });
+
+module.exports = router;
